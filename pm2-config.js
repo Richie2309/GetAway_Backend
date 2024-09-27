830327fb6c0a7c3966e279c53d2eac6c28e29cfd
Richin Rajeev T R <richinrajeev796@gmail.com>
2024-09-27 12:59:02 +0530

    type removed

--- pm2-config.js
+++ pm2-config.js
@@ -1,18 +1,19 @@
 module.exports = {
   apps: [
-      {
-          name: 'backend',
-          script: './src/server.ts',
-          watch: true,
-          ignore_watch: ['node_modules', 'logs'],
-          instances: 1,
-          exec_mode: 'fork',
-          autorestart: true,
-          max_restarts: 10,
-          env: {
-              NODE_ENV: 'production',
-              PORT: 3000,
-          },
-      },
-    ],
+    {
+      name: 'backend',
+      script: './src/server.ts',
+      watch: true,
+      ignore_watch: ['node_modules', 'logs'],
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      max_restarts: 10,
+      interpreter: 'ts-node',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
 };
